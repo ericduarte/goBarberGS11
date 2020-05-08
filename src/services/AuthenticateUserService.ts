dc7abbc61e157a1cb8cc15677c232d1992aa9039
Eric Duarte <ericduartemail@gmail.com>
2020-05-08 10:51:02 -0300

    Gerando Token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,16 @@ class AuthenticateUserService {
       throw new Error('Invalid credentials');
     }
 
+    const user_id = user.id;
+
+    const token = sign({ user_id, email }, '8c58ee7064ec8891af02ec8185d3ca1f', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
