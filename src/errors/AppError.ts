08b3d7445d602b34384cc2c3986f5351717dc221
Eric Duarte <ericduartemail@gmail.com>
2020-05-08 15:24:12 -0300

    Lidando com erros

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,11 +1,11 @@
 class AppError {
   public readonly message: string;
 
-  public readonly statuCode: number;
+  public readonly statusCode: number;
 
   constructor(message: string, statusCode = 400) {
     this.message = message;
-    this.statuCode = statusCode;
+    this.statusCode = statusCode;
   }
 }
 
