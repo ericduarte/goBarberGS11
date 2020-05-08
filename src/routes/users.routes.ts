396057d27c49de13b317da3f1c10beb9d195ca6c
Eric Duarte <ericduartemail@gmail.com>
2020-05-08 14:44:46 -0300

    Atualizando o Avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { UpdateDateColumn } from 'typeorm';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -34,7 +36,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
     return response.json({ ok: true });
   },
 );
