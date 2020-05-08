2c54fa4b09bf98aebea96fe96a62b1d1d15315c1
Eric Duarte <ericduartemail@gmail.com>
2020-05-08 08:33:39 -0300

    Migrations usuarios

--- src/database/migrations/1588883740384-CreateUsers.ts
+++ src/database/migrations/1588883740384-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1588875743476
-  implements MigrationInterface {
+export default class CreateUsers1588883740384 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,14 +14,18 @@ export default class CreateAppointments1588875743476
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
             isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -40,6 +43,6 @@ export default class CreateAppointments1588875743476
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
