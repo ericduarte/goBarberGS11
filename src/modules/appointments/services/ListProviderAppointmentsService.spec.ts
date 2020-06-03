c57a30ac66856105ab8efcf7aaee454c73f36da9
Eric Duarte <ericduartemail@gmail.com>
2020-06-03 17:19:26 -0300

    ListProviderAppointments

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,40 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointmentsService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list appointment on a specific day', async () => {
+    const insertedAppointments: Appointment[] = [];
+    // eslint-disable-next-line no-plusplus
+    for (let hour = 8; hour <= 12; hour++) {
+      // eslint-disable-next-line no-await-in-loop
+      const appointment = await fakeAppointmentsRepository.create({
+        date: new Date(2020, 4, 20, hour, 0, 0),
+        provider_id: 'provider',
+        user_id: 'user1',
+      });
+      insertedAppointments.push(appointment);
+    }
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      year: 2020,
+      month: 5,
+      day: 20,
+    });
+
+    expect(appointments).toEqual(
+      expect.arrayContaining([...insertedAppointments]),
+    );
+  });
+});
